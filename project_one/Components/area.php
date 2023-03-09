8f8f47b1df450f8b13575d5a6d967cdc44ddd6f7
Sudarshani Kshirsagar <srk29250@gmail.com>
2023-03-09 13:56:16 +0530

    success

--- project_one/Components/area.php
+++ project_one/Components/area.php
@@ -1,7 +1,7 @@
 <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9" style="width=100%;">
     <div class="panel panel-primary">
         <div class="panel-heading">
-            <h3 class="panel-title">City</h3>
+            <h3 class="panel-title">Area</h3>
         </div>
         <div class="panel-body">
             <form action="saveproject.php" method="POST" role="form">
@@ -10,7 +10,7 @@
                     <input type="state" class="form-control" id="" name="txtid" placeholder="Input field">
                 </div>
                 <div class="form-group">
-                    <label for="">City</label>
+                    <label for="">Area</label>
                     <input type="state" class="form-control" id="" name="txtid" placeholder="Input field">
                 </div>
                 <button type="submit" class="btn btn-primary" name="submit">Save</button>
