be8a6e8cdbe7cf5b03e580a19f5ca4e42ec2dc2f
pushkraj <dev.pushkraj@gmail.com>
2023-03-19 09:53:23 +0530

    123

--- project_one/Side_Menu/master.php
+++ project_one/Side_Menu/master.php
@@ -1,8 +1,10 @@
-<?php
-if(isset($_COOKIE["myCookie"])) {
-    $value = $_COOKIE["myCookie"];
-    echo "Cookie value is: " . $value;
-} else {
-    echo "Cookie not set.";
-}
-?>
+<ul class="list-group">
+      <button type="button" class="btn btn-info full-width" data-toggle="collapse" data-target="#demo">Master</button>
+      <div id="demo" class="collapse">
+            <li class="center_text list-group-item"><a href="index.php">HOME</a></li>
+            <!-- <li class="center_text list-group-item"><a href="index.php?master=usertype">User Type</a></li> -->
+            <li class="center_text list-group-item"><a href="index.php?master=state">State</a></li>
+            <li class="center_text list-group-item"><a href="index.php?master=city">City</a></li>
+            <li class="center_text list-group-item"><a href="index.php?master=area">Area</a></li>
+      </div>
+</ul>
